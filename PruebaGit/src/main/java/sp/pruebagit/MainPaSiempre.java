9f4db0c442d045f71e3a36dcdbd818bc7761dd3e
mcarr <mcarr@LAPTOP-L8HM1LUB>
2021-02-09 21:27:04 +0100

    mensaje muy importante

---
+++ PruebaGit/src/main/java/sp/pruebagit/MainPaSiempre.java
@@ -0,0 +1,17 @@
+package sp.pruebagit;
+
+/**
+ *
+ * @author mcarr
+ */
+public class MainPaSiempre {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Mensaje muy importante
+        System.out.println("Do not change main branch");
+    }
+    
+}
